f796cae663562b584ecf07d634b57d5c518597fd
Bigalan09 <Bigalan09@users.noreply.github.com>
2026-04-01 01:29:04 +0000

    Deploy preview for PR 136 🛫

---
+++ pr-preview/pr-136/config.js
@@ -0,0 +1,8 @@
+window.BUROHAME_RUNTIME_CONFIG = {
+  progressionResetDate: '2026-03-28',
+  weeklyLeaderboard: {
+    backend: 'local',
+    supabaseUrl: '',
+    supabasePublishableKey: '',
+  },
+};
